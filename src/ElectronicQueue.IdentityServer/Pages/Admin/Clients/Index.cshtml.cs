ff82827b73358768efe1f16f0d6ee00f26a2296f
WRNGx3 <WRNGx3@LAPTOP-AZ-002>
2025-12-21 09:16:40 +0300

    Add project files.

---
+++ src/ElectronicQueue.IdentityServer/Pages/Admin/Clients/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElectronicQueue.IdentityServer.Pages.Admin.Clients
+{
+    [SecurityHeaders]
+    [Authorize(Config.Policies.Admin)]
+    public class IndexModel(ClientRepository repository) : PageModel
+    {
+        public IEnumerable<ClientSummaryModel> Clients { get; private set; } = default!;
+        public string? Filter { get; set; }
+
+        public async Task OnGetAsync(string? filter)
+        {
+            Filter = filter;
+            Clients = await repository.GetAllAsync(filter);
+        }
+    }
+}
